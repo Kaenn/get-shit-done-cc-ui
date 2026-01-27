2a81242cf61fad332e6b0b577986bd10d0935dfe
Kaenn <glenninizan@gmail.com>
2026-01-27 16:31:28 -0600

    fix(npm): resolve package path correctly for npx symlinks

--- npm/bin/get-shit-done-cc-ui.cmd
+++ npm/bin/get-shit-done-cc-ui.cmd
@@ -1,11 +1,21 @@
 @echo off
 setlocal
+
+:: Find the package directory (resolve symlinks)
 set "SCRIPT_DIR=%~dp0"
-set "BINARY=%SCRIPT_DIR%gsd-ui-web.exe"
+set "PACKAGE_DIR=%SCRIPT_DIR%.."
+
+:: If we're in node_modules/.bin, find the actual package
+if exist "%PACKAGE_DIR%\get-shit-done-cc-ui\package.json" (
+  set "PACKAGE_DIR=%PACKAGE_DIR%\get-shit-done-cc-ui"
+)
+
+set "BINARY=%PACKAGE_DIR%\bin\gsd-ui-web.exe"
+set "DOWNLOAD_SCRIPT=%PACKAGE_DIR%\lib\download.js"
 
 if not exist "%BINARY%" (
   echo Binary not found. Downloading for your platform...
-  node "%SCRIPT_DIR%..\lib\download.js"
+  node "%DOWNLOAD_SCRIPT%"
   if errorlevel 1 exit /b 1
   echo.
   echo Download complete! Run 'npx get-shit-done-cc-ui' again to start.
